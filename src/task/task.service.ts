a2ef2f4811296e4a7855773361daf5e3f39a19e9
Philip Veremeichik <p.veremeichik@richbrains.net>
2026-01-27 16:31:32 +0300

    fix

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -490,7 +490,7 @@ export class TaskService {
     task.status = TaskStatus.ACTIVE;
     task.approveBy = [];
 
-    const savedTask = await this.taskRepository.update(task.id, task);
+    await this.taskRepository.update(task.id, task);
 
     // Отправляем уведомление волонтеру об отмене назначения
     if (previousVolunteerId) {
@@ -509,7 +509,7 @@ export class TaskService {
         });
     }
 
-    return savedTask;
+    return task;
   }
 
   async approveCompletion(
