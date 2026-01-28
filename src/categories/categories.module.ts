a6c460ec6fcfaf1c0d9df83cd32cf0493a85806b
Philip Veremeichik <p.veremeichik@richbrains.net>
2026-01-28 14:22:13 +0300

    add remove

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
 import { Category } from './entities/category.entity';
+import { Skill } from 'src/skills/entities/skill.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Category])],
+  imports: [TypeOrmModule.forFeature([Category, Skill])],
   controllers: [CategoriesController],
   providers: [CategoriesService],
   exports: [CategoriesService, TypeOrmModule],
