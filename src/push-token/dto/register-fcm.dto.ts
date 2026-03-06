51a82567f0aa2d1ad79cd8a9b2317575e4ee606d
Nick Shuminskiy <shuminskiy23@gmail.com>
2026-03-07 01:43:44 +0300

    update(RegisterFcmDto): add 'web' option to platform enum for FCM registration

--- src/push-token/dto/register-fcm.dto.ts
+++ src/push-token/dto/register-fcm.dto.ts
@@ -6,9 +6,9 @@ export class RegisterFcmDto {
   @IsString()
   token: string;
 
-  @ApiProperty({ description: 'Platform', enum: ['ios', 'android'] })
+  @ApiProperty({ description: 'Platform', enum: ['ios', 'android', 'web'] })
   @IsString()
-  @IsIn(['ios', 'android'])
+  @IsIn(['ios', 'android', 'web'])
   platform: string;
 
   @ApiPropertyOptional({ description: 'Device identifier' })
